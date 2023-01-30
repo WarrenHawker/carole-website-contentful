c93d99ac6f1895d1246a5741d7d6e09bb7eb6c9b
WarrenHawker <hawker.warren@gmail.com>
2023-01-30 11:56:56 +0000

    styled header and mobile nav

--- src/components/header.js
+++ src/components/header.js
@@ -9,44 +9,48 @@ const Header = () => {
   return (
     <header>
       <h1>Carole Tongue</h1>
-      <div
-        id='nav-icon'
-        className={navShow ? 'open' : ''}
-        onClick={() => setNavShow(!navShow)}>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <nav className={navShow ? 'active' : ''}>
-        <Link
-          className={
-            currentRoute === '/' ? 'top-nav-link active' : 'top-nav-link'
-          }
-          href='/'>
-          Home
-        </Link>
-        <Link
-          className={
-            currentRoute === '/about' ? 'top-nav-link active' : 'top-nav-link'
-          }
-          href='/about'>
-          About Me
-        </Link>
-        <Link
-          className={
-            currentRoute === '/art' ? 'top-nav-link active' : 'top-nav-link'
-          }
-          href='/art'>
-          Art
-        </Link>
-        <Link
-          className={
-            currentRoute === '/contact' ? 'top-nav-link active' : 'top-nav-link'
-          }
-          href='/contact'>
-          Contact
-        </Link>
+      <nav>
+        <div
+          id='nav-icon'
+          className={navShow ? 'open' : ''}
+          onClick={() => setNavShow(!navShow)}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className={navShow ? 'nav-links active' : 'nav-links'}>
+          <Link
+            className={
+              currentRoute === '/' ? 'top-nav-link active' : 'top-nav-link'
+            }
+            href='/'>
+            Home
+          </Link>
+          <Link
+            className={
+              currentRoute === '/about' ? 'top-nav-link active' : 'top-nav-link'
+            }
+            href='/about'>
+            About Me
+          </Link>
+          <Link
+            className={
+              currentRoute === '/art' ? 'top-nav-link active' : 'top-nav-link'
+            }
+            href='/art'>
+            Art
+          </Link>
+          <Link
+            className={
+              currentRoute === '/contact'
+                ? 'top-nav-link active'
+                : 'top-nav-link'
+            }
+            href='/contact'>
+            Contact
+          </Link>
+        </div>
       </nav>
     </header>
   );
